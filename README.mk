544e1005afa219d45906f47a90d32cc734790fa9
bruno <65568046+yconi@users.noreply.github.com>
2026-08-27 13:33:50 -0300

    First README.mk

---
+++ README.mk
@@ -0,0 +1,17 @@
+# Linear Regression
+The linear regression is a model that estimates the relashionship between a  independent variable and a dependent variable.
+## Main objective
+Understanding the basic of regression models. In this moment, the objective is not to build the best model ever, but to learn more about basic statistics models.
+
+## model.py
+At this moment, the model receives an array with some ordered pair as the data for training.
+``` py
+exampleData = [0, 4.3], [1, 7.4], [2, 8.3], [3, 10.7], [4, 13.1]
+```
+After choosing the desired array, the model finds the slope __*m*__ and the intersection __*b*__ to create the first basic function.
+$$
+m=\frac{ΔY}{ΔX}
+$$
+$$
+b=y1[n] -mx[n]
+$$
